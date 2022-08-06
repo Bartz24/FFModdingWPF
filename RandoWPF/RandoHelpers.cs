128da834aeed44bd6033be79a2c44698887d425f
Bartz24 <bartz24games@gmail.com>
2022-08-06 17:25:42 -0400

    FF13 include seed in modpack name
    Former-commit-id: ef1d87bf1bb920e0e33e8dbb5bc702a6016d40b8

---
+++ RandoWPF/RandoHelpers.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace Bartz24.RandoWPF
+{
+    public class RandoHelpers
+    {
+        public static void UpdateSeedInFile(string file, string seed)
+        {
+            string text = File.ReadAllText(file);
+            text = text.Replace("_SEED_", seed);
+            File.WriteAllText(file, text);
+        }
+    }
+}
