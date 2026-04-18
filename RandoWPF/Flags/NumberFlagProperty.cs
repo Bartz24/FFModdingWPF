f03431c1f95792781a209446a73b68bc91b063f4
Bartz24 <bartz24games@gmail.com>
2026-04-18 01:39:06 -0400

    Add NumberFlagProperty logarithmic scale

--- RandoWPF/Flags/NumberFlagProperty.cs
+++ RandoWPF/Flags/NumberFlagProperty.cs
@@ -8,6 +8,12 @@ namespace Bartz24.RandoWPF;
 [JsonObject(MemberSerialization.OptIn)]
 public class NumberFlagProperty : FlagProperty
 {
+    public enum NumberScaleType
+    {
+        Linear,
+        Logarithmic
+    }
+
     public NumberFlagProperty(int defaultValue) : base(defaultValue)
     {
         value = defaultValue;
@@ -32,8 +38,24 @@ public class NumberFlagProperty : FlagProperty
     public int MaxValue { get; set; }
     public int MinValue { get; set; }
     public int StepSize { get; set; } = 1;
+    public NumberScaleType ScaleType { get; set; } = NumberScaleType.Linear;
+    public int LogarithmicBase { get; set; } = 10;
 
     public string ValueText { get; set; } = "Value:";
+
+    public double SliderMinimum => ScaleType == NumberScaleType.Logarithmic ? GetLogarithmicPosition(MinValue) : MinValue;
+    public double SliderMaximum => ScaleType == NumberScaleType.Logarithmic ? GetLogarithmicPosition(MaxValue) : MaxValue;
+    public double SliderTickFrequency => ScaleType == NumberScaleType.Logarithmic ? 1 : StepSize;
+
+    public double SliderValue
+    {
+        get => ScaleType == NumberScaleType.Logarithmic ? GetLogarithmicPosition(Value) : Value;
+        set
+        {
+            Value = ScaleType == NumberScaleType.Logarithmic ? GetLogarithmicValue(value) : (int)Math.Round(value);
+            OnPropertyChanged(new PropertyChangedEventArgs(nameof(SliderValue)));
+        }
+    }
 
     private int value;
     [JsonProperty]
@@ -50,15 +72,40 @@ public class NumberFlagProperty : FlagProperty
         }
         set
         {
-            this.value = value;
+            this.value = ClampValue(value);
 
             OnPropertyChanged(new PropertyChangedEventArgs(nameof(Value)));
+            OnPropertyChanged(new PropertyChangedEventArgs(nameof(SliderValue)));
         }
     }
 
     public override void Deserialize(IDictionary<string, object> data)
     {
-        // Limit the value to be within min/max range
-        Value = Math.Max(MinValue, Math.Min(MaxValue, (int)(long)data["Value"]));
+        Value = (int)(long)data["Value"];
+    }
+
+    private int ClampValue(int newValue)
+    {
+        int clamped = Math.Max(MinValue, Math.Min(MaxValue, newValue));
+        if (ScaleType != NumberScaleType.Logarithmic)
+        {
+            return clamped;
+        }
+
+        double position = GetLogarithmicPosition(clamped);
+        return GetLogarithmicValue(position);
+    }
+
+    private double GetLogarithmicPosition(int currentValue)
+    {
+        double logBase = Math.Log(LogarithmicBase);
+        return Math.Round(Math.Log(currentValue) / logBase);
+    }
+
+    private int GetLogarithmicValue(double sliderPosition)
+    {
+        double roundedPosition = Math.Round(sliderPosition);
+        double result = Math.Pow(LogarithmicBase, roundedPosition);
+        return (int)Math.Round(result);
     }
 }
